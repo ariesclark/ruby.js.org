7228d82442dc9be37da0977ef452ec0461ff41aa
rubybb <10256477+rubybb@users.noreply.github.com>
2020-09-05 03:06:15 -0600

    designed and wrote entire site.

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es2020": true
+	},
+	"extends": [
+		"eslint:recommended",
+	],
+	"parserOptions": {
+		"ecmaVersion": 11,
+		"sourceType": "module"
+	},
+	"plugins": [],
+	"rules": {
+
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	}
+};
